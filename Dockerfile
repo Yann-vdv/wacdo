dbf0b208aac68360f1c8129c4800ccd80c2f4a5d
Yann-vdv <yann59496@gmail.com>
2025-09-25 16:50:35 +0200

    prepare build
    -java 24 to java 21 (more stable)
    -access to remote db
    -add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM jelastic/maven:3.9.5-openjdk-21 AS BUILD
+COPY . .
+RUN mvn clean install
+
+FROM eclipse-temurin:21
+COPY --from=build /target/wacdo-0.0.1-SNAPSHOT.jar wacdo.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","wacdo.jar"]
--- src/main/java/com/gdu/wacdo/WacdoApplication.java
+++ src/main/java/com/gdu/wacdo/WacdoApplication.java
@@ -3,10 +3,19 @@ package com.gdu.wacdo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Objects;
+
 @SpringBootApplication
 public class WacdoApplication {
 
 	public static void main(String[] args) {
+        String trustStorePassword = System.getProperty("custom.truststore.password", "W@cdoP@ss");
+
+        String trustStorePath = Objects.requireNonNull(WacdoApplication.class.getResource("/ca-truststore.jks")).getPath();
+        System.setProperty("javax.net.ssl.trustStore", trustStorePath);
+        System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+
 		SpringApplication.run(WacdoApplication.class, args);
 	}
 
